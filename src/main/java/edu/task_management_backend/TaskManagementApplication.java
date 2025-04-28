2951f8f95a863deae80569101497655742d52ddf
Kalum Gunawardhana <kguna415@gmail.com>
2025-04-28 08:33:04 +0530

    first commit

---
+++ src/main/java/edu/task_management_backend/TaskManagementApplication.java
@@ -0,0 +1,11 @@
+package edu.task_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/task_management_backend/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.task_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
